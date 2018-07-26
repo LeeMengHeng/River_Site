afca71b56d0e9006ac17ef77e684077d03eaecf9
LeeMengHeng <leedonny123@gmail.com>
2018-07-26 17:23:11 +0800

    20180726 init

---
+++ main.js
@@ -0,0 +1,90 @@
+var svg = d3.select("svg");
+var projection = d3.geoMercator().scale(80)
+var path = d3.geoPath(projection);
+// d3.json("https://unpkg.com/world-atlas@1/world/110m.json", function(err, data){
+//   if (err) throw err;
+//   var feature = topojson.feature(data, data.objects.countries);
+//   svg.append("path")
+//     .datum(feature)
+//     .attr("d",path)
+//     .style("fill","none")
+//     .style("stroke","lightblue")
+// });
+
+
+d3.queue()
+.defer(d3.json, "https://unpkg.com/world-atlas@1/world/110m.json")
+.defer(d3.csv, "River_Info.csv", function(d){
+  // console.log(d)
+  // return d;
+  var r = {
+    name:d.Catchment,
+    country:d.Country,
+    coord: projection([d.Longitude, d.Latitude]),
+  };
+  ["LowerMassInputEstimate","MidpointMassInputEstimate","UpperMassInputEstimate","TotalCatchmentSurfaceArea","YearlyAverageDischarge"].forEach(function(g){
+    var num=d[g];
+    r[g]= [num.slice(0,num.length-1),num[num.length-1]];
+  });
+  console.log(r)
+  return r;
+})
+.await(function(err, world, rivers){
+  if (err) throw err;
+  var feature = topojson.feature(world, world.objects.countries);
+  svg.append("path")
+    .datum(feature)
+    .attr("d",path)
+    .style("fill","none")
+    .style("stroke","lightblue");
+
+  svg.selectAll("circle")
+    .data(rivers)
+    .enter()
+    .append("circle")
+    .attr("cx",function(d){
+      return d.coord[0]
+    })
+    .attr("cy",function(d){
+      return d.coord[1]
+    })
+      .attr("r", 3)
+      .style("fill", "pink");
+})
+
+
+
+
+// d3.csv(
+//   "River_Info.csv",
+//   function(d){
+//     // console.log(d)
+//     // return d;
+//     var r = {
+//       name:d.Catchment,
+//       country:d.Country,
+//       coord: projection([d.Longitude, d.Latitude]),
+//     };
+//     ["LowerMassInputEstimate","MidpointMassInputEstimate","UpperMassInputEstimate","TotalCatchmentSurfaceArea","YearlyAverageDischarge"].forEach(function(g){
+//       var num=d[g];
+//       r[g]= [num.slice(0,num.length-1),num[num.length-1]];
+//     });
+//     console.log(r)
+//     return r;
+//   },
+//   function(err,data){
+//     if(err) throw err;
+//     svg.selectAll("circle")
+//       .data(data)
+//       .enter()
+//       .append("circle")
+//       .attr("cx",function(d){
+//         return d.coord[0]
+//       })
+//       .attr("cy",function(d){
+//         return d.coord[1]
+//       })
+//       .attr("r", 3)
+//       .style("fill", "pink");
+//   }
+// );
